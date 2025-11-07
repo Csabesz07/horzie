74f549f7817957768015fb58fd4a9cecd7c85ee6
Szilágyi Csaba <csabi32105@gmail.com>
2025-11-07 08:32:50 +0100

    Creating pixi environment

--- horzie.py
+++ horzie.py
@@ -4,3 +4,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from IPython.display import Image as IMG
