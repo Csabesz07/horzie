164472ee971e67242555ebc13c7670881f0289ed
Szilágyi Csaba <csabi32105@gmail.com>
2025-11-11 19:10:42 +0100

    Correlation matrix

--- horzie.py
+++ horzie.py
@@ -43,6 +43,7 @@ place_match = {
   '11.':11,
   '12.':12,
 }
+
 def place_conversion(x):
   for [key, val] in place_match.items():
     if key in str(x):
@@ -50,13 +51,16 @@ def place_conversion(x):
       return x
   return str(x)
 
+def normalize(x):
+  return (x - x.min()) / (x.max() - x.min())
+
 '''
 The magic itself
 '''
 
 results = pd.read_csv('./races_2003_2025.csv')
 
-# print(results.shape) # Uncomment to see the shape of the result set
+print(results.shape)
 
 results.drop(columns=[
   'date',
@@ -73,22 +77,31 @@ results.drop(columns=[
   inplace=True)
 
 # @TODO Try to fill the nullish values (race_time), instead of masking
-
-mask = results['race_time'].notna()
+# print(results.isnull().sum()) # Should check nan as well
 
 results['race_of_the_day'] = results['race_of_the_day'].apply(place_conversion)
-results['place'] = results['place'].apply(place_conversion)
 
+
+results['place'] = results['place'].apply(place_conversion)
 results['place'] = pd.to_numeric(results['place'], errors='coerce')
+
+mask = results['race_time'].notna()
 results['race_time'] = results.loc[mask, 'race_time'] = pd.to_timedelta(
     '00:' + results.loc[mask, 'race_time'].astype(str)
 )
+results['race_time'] = results['race_time'].dt.total_seconds()
+results[['race_time', 'distance', 'dividend']] = results[['race_time', 'distance', 'dividend']].apply(normalize)
+
+# @TODO Try to convert string columns like trainer to a numeric value, as it might mean mething who trained the horses
 
 print(results.info())
 pd.set_option('display.max_colwidth', None)
 print(results.head())
 
-# sns.set_theme(rc = {'figure.figsize':(20,20), 'font.weight': 'bold', 'font.size': 12, 'xtick.labelsize': 14, 'ytick.labelsize': 14, 'xtick.top': True, 'xtick.labeltop': True})
-# sns.set_theme(context='notebook', style='darkgrid', palette='deep', font='sans-serif', font_scale=1, color_codes=True, rc=None)
-# ax = sns.heatmap(results.corr().round(2), annot=True, cmap="coolwarm")
-# plt.savefig('corr.png', bbox_inches='tight')
+X = results
+Y = results['place']
+
+sns.set_theme(rc = {'figure.figsize':(20,20), 'font.weight': 'bold', 'font.size': 12, 'xtick.labelsize': 14, 'ytick.labelsize': 14, 'xtick.top': True, 'xtick.labeltop': True})
+sns.set_theme(context='notebook', style='darkgrid', palette='deep', font='sans-serif', font_scale=1, color_codes=True, rc=None)
+ax = sns.heatmap(results.corr(numeric_only=True).round(2), annot=True, cmap="coolwarm")
+plt.savefig('corr.png', bbox_inches='tight')
